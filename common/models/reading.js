c514d289c794a2e021d39dbca34055a1bd46cada
Dan Lively <dan@danlively.com>
2015-10-22 23:10:42 -0400

    add remote method for latest

--- common/models/reading.js
+++ common/models/reading.js
@@ -1,3 +1,29 @@
 module.exports = function(Reading) {
 
+  Reading.beforeRemote('create', function(context, user, next) {
+    var req = context.req;
+    req.body.reading_time = Date.now();
+    next();
+  });
+
+  Reading.latest = function(cb) {
+
+  Reading.find({limit: 1, order: 'reading_time DESC'}, function(err, temperature) {
+    cb(null, temperature[0]);
+  });
+
+
+ };
+
+
+ Reading.remoteMethod(
+   'latest',
+   {
+     http: {path: '/latest', verb: 'get'},
+     returns: [
+       {arg: 'temperature', type: 'object', root: true}
+     ]
+   }
+ );
+
 };
